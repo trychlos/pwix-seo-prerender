7f1fd2f5dc757f9343f3f3c602fd3a8a7c24c71f
Pierre Wieser <p.wieser@trychlos.org>
2026-02-11 09:43:00 +0100

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:seo-prerender',
-    version: '1.0.1',
+    version: '1.0.2-rc.0',
     // Brief, one-line summary of the package.
     summary: 'A Meteor package which manages static prerendering for SEO puposes.',
     // URL to the Git repository containing the source code for this package.
